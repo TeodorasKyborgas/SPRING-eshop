faeebd24a7ca7a2817191f1364477f41a2077822
Deivdas Radvila <deividas.radvila@gmail.com>
2024-01-14 19:38:54 +0200

    Added dev dependencies (lombok+ conf. processor + dev tools)

--- src/main/java/lt/codeacademy/eshop/FirstControler.java
+++ src/main/java/lt/codeacademy/eshop/FirstControler.java
@@ -2,14 +2,12 @@ package lt.codeacademy.eshop;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstControler {
 
-    @GetMapping("/hello/{name}")
-    public String sayHelloToCustomer(@PathVariable String name, @RequestParam String surename){
+    @GetMapping("/hello")
+    public String sayHelloToCustomer(){
         return "hello";
     }
 }
