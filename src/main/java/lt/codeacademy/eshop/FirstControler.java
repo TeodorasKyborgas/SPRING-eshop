c942ee05fe66ee875c42b05a650f740c8089f8b1
Deivdas Radvila <deividas.radvila@gmail.com>
2024-01-14 20:24:21 +0200

    Product creation via form and http pos

--- src/main/java/lt/codeacademy/eshop/FirstControler.java
+++ src/main/java/lt/codeacademy/eshop/FirstControler.java
@@ -1,13 +1,21 @@
 package lt.codeacademy.eshop;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FirstControler {
 
-    @GetMapping("/hello")
-    public String sayHelloToCustomer(){
+    @GetMapping("/products/create")
+    public String sayHelloToCustomer(Model model){
+        model.addAttribute("product", new Product());
+        return "product";
+    }
+    @PostMapping("/products/create")
+    public String crateAProduct(Product product){
         return "hello";
     }
 }
