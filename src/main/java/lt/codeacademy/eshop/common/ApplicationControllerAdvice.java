32f16345917aa4e311465e30ae7386684d0d259c
Deivdas Radvila <deividas.radvila@gmail.com>
2024-02-17 23:39:49 +0200

    Errors. IninBinder trim spaces for string text

--- src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
+++ src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
@@ -1,9 +1,12 @@
 package lt.codeacademy.eshop.common;
 
 import lt.codeacademy.eshop.product.exception.ProductNotFoundException;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 
 @ControllerAdvice
 
@@ -13,5 +16,11 @@ public class ApplicationControllerAdvice {
     public String productNotFound(ProductNotFoundException e, Model model) {
         model.addAttribute("productUUID", e.getProductUUID());
         return "product/error/productNotFound";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, trimmerEditor);
     }
 }
