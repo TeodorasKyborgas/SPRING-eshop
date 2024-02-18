d9d44dec94905f9d4524d2ca46b1f0297dc352e2
Deivdas Radvila <deividas.radvila@gmail.com>
2024-02-18 18:32:26 +0200

    added @modelAttribute to create default value + hard coded card template from controller product dto

--- src/main/java/lt/codeacademy/eshop/cart/CartController.java
+++ src/main/java/lt/codeacademy/eshop/cart/CartController.java
@@ -1,16 +1,28 @@
 package lt.codeacademy.eshop.cart;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lt.codeacademy.eshop.product.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart")
 @SessionAttributes("cartSession")
 public class CartController {
+
+    @ModelAttribute("cartSession")
+    public List<ProductDto> createDefaultCartSession() {
+        return List.of(ProductDto.builder()
+                .name("Testas")
+                .build());
+    }
+
 
     @GetMapping
     public String openCart(HttpServletRequest request, Model model) {
@@ -18,9 +30,4 @@ public class CartController {
         return "/cart/cart";
     }
 
-    @GetMapping("/add")
-    public String openCart() {
-
-        return "redirect:/cart";
-    }
 }
