96799e0aacfa054ebda7edff12f247d1fd821f05
Deivdas Radvila <deividas.radvila@gmail.com>
2024-02-18 19:57:56 +0200

    added simple registration form

--- src/main/java/lt/codeacademy/eshop/product/dto/ProductDto.java
+++ src/main/java/lt/codeacademy/eshop/product/dto/ProductDto.java
@@ -3,6 +3,7 @@ package lt.codeacademy.eshop.product.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,5 +27,6 @@ public class ProductDto {
     private int amount;
 
     @NotNull
+    @Size
     private Long categoryId;
 }
