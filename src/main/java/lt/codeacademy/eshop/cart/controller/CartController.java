c5427147d1f8fbaf328226c64d61e93a0af65481
Deivdas Radvila <deividas.radvila@gmail.com>
2024-02-21 20:45:25 +0200

    make session attribute complete (removed)

--- src/main/java/lt/codeacademy/eshop/cart/controller/CartController.java
+++ src/main/java/lt/codeacademy/eshop/cart/controller/CartController.java
@@ -6,6 +6,7 @@ import lt.codeacademy.eshop.cart.dto.CartDto;
 import lt.codeacademy.eshop.cart.service.CartService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.UUID;
 
@@ -31,6 +32,14 @@ public class CartController {
     public String addToCart(@PathVariable UUID productId,
                             @ModelAttribute("cartSession") CartDto cart) {
         cartService.addProductToCartByProductId(productId, cart);
+
+        return "redirect:/products";
+    }
+    @PostMapping
+    public String order(SessionStatus sessionStatus) {
+        //TODO: save into DB or do other things with cart data
+
+        sessionStatus.setComplete();
 
         return "redirect:/products";
     }
