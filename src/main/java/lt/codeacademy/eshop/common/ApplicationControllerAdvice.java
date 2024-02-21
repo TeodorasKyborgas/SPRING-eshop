4d997619244c418fd786ec4040b8580739955047
Deivdas Radvila <deividas.radvila@gmail.com>
2024-02-21 20:42:40 +0200

    used global currency in new created GlobaAttributeAdvice +cart translations

--- src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
+++ src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.Date;
 
 @ControllerAdvice
 
@@ -25,11 +22,6 @@ public class ApplicationControllerAdvice {
     public void initBinder(WebDataBinder webDataBinder) {
         StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
         webDataBinder.registerCustomEditor(String.class, trimmerEditor);
-    }
-
-    @ModelAttribute("modelDateAttr")  // modelDate is optional here, HTML will use now (as method name)
-    public Date now() {
-        return new Date();
     }
 
 }
