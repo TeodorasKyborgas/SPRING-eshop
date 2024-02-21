747f8c9332c358939bc0476c0f932f96293ad612
Deivdas Radvila <deividas.radvila@gmail.com>
2024-02-21 20:24:07 +0200

    increment same product to one line on the cart view

--- src/main/java/lt/codeacademy/eshop/cart/dto/CartDto.java
+++ src/main/java/lt/codeacademy/eshop/cart/dto/CartDto.java
@@ -13,6 +13,8 @@ public class CartDto {
     public void add(final ProductDto productDto) {
         cartItems.add(CartItemDto.builder()
                 .productDto(productDto)
+                .quantity(1)
                 .build());
+
     }
 }
