d6edb841f20688d4a974c1fc84a06801f08a5321
Deivdas Radvila <deividas.radvila@gmail.com>
2024-02-24 12:33:26 +0200

    fixed h2 using websecuritycustomizer static files fixed secure add, delete and update files

---
+++ src/main/java/lt/codeacademy/eshop/security/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package lt.codeacademy.eshop.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login/**").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(loginConfigure -> loginConfigure
+                        .permitAll()
+                        .loginPage("/login")                //GET - the login form
+                        .loginProcessingUrl("/login")       //Specifies the URL to validate the credentials.
+                        .defaultSuccessUrl("/products", true)
+                        .usernameParameter("loginEmail")    //The HTTP parameter to look for the username
+                        .passwordParameter("loginPassword") //The HTTP parameter to look for the password
+                )
+
+                .build();
+    }
+}
